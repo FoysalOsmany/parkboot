8f368f5a527a7d974757636beb16145ef2fe303f
FoysalOsmany <foysalo@gmail.com>
2019-10-22 20:23:09 +0600

    ParkingLot Repository

---
+++ src/main/java/com/park/parkboot/components/parkinglot/ParkingLotRepository.java
@@ -0,0 +1,7 @@
+package com.park.parkboot.components.parkinglot;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ParkingLotRepository extends PagingAndSortingRepository<ParkingLot, Integer> {
+
+}
