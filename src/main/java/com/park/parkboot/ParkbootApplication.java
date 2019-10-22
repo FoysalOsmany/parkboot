f8f21fe08c7f59edb7073b4f9371f5be0490a6ef
FoysalOsmany <foysalo@gmail.com>
2019-10-22 18:26:03 +0600

    Main application

---
+++ src/main/java/com/park/parkboot/ParkbootApplication.java
@@ -0,0 +1,13 @@
+package com.park.parkboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkbootApplication.class, args);
+	}
+
+}
