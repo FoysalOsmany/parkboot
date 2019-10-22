841c0681314069c3c2c87ee68ab2598261673f66
FoysalOsmany <foysalo@gmail.com>
2019-10-22 21:47:12 +0600

    remove unused code from parking lot service

--- src/main/java/com/park/parkboot/components/parkinglot/ParkingLotService.java
+++ src/main/java/com/park/parkboot/components/parkinglot/ParkingLotService.java
@@ -1,10 +1,7 @@
 package com.park.parkboot.components.parkinglot;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ParkingLotService {
@@ -14,26 +11,4 @@ public class ParkingLotService {
     public ParkingLotService(ParkingLotRepository parkingLotRepository) {
         this.parkingLotRepository = parkingLotRepository;
     }
-
-    public ParkingLot addParkingLot(@Valid @RequestBody ParkingLot parkingLotModel) {
-        return this.parkingLotRepository.save(parkingLotModel);
-    }
 } 
-
-
-// @RestController
-//  @RequestMapping("/api/parkingLots")
-// public class ParkingLotController {
-//     @Autowired
-//     private ParkingLotRepository parkingLotRepository;
-
-//     @GetMapping("/")
-//     public List<ParkingLotModel> getAllParkingLot() {
-//         return parkingLotRepository.findAll();
-//     }
-    
-//     @PostMapping("/")
-//     public ParkingLotModel enlistParkingLot(@Valid @RequestBody ParkingLotModel parkingLotModel) {
-//         return parkingLotRepository.save(parkingLotModel);
-//     }
-// }
