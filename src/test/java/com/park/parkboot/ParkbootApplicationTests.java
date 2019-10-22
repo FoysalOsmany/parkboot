ab3202f1a7f632d3af014adfe0116e8395392fbb
FoysalOsmany <foysalo@gmail.com>
2019-10-22 18:26:13 +0600

    Test file

---
+++ src/test/java/com/park/parkboot/ParkbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.parkboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
