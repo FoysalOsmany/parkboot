042264963c2cdee0fdca53e32393ecdd713bf897
FoysalOsmany <foysalo@gmail.com>
2019-10-23 19:39:37 +0600

    find by license number

--- src/main/java/com/park/parkboot/components/car/CarRepository.java
+++ src/main/java/com/park/parkboot/components/car/CarRepository.java
@@ -3,5 +3,5 @@ package com.park.parkboot.components.car;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CarRepository extends PagingAndSortingRepository<Car, Integer> {
-
+    public Car findByLicenseNumber(String licenseNumber);
 }
