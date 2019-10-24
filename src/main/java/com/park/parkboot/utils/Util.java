60001465a7e0b510e9eb5ae7389a74650709455e
FoysalOsmany <foysalo@gmail.com>
2019-10-24 15:17:59 +0600

    added util method

--- src/main/java/com/park/parkboot/utils/Util.java
+++ src/main/java/com/park/parkboot/utils/Util.java
@@ -3,9 +3,23 @@ package com.park.parkboot.utils;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class Util {
     public static long getTimeDiff(Date toDate, Date fromDate, TimeUnit timeUnit) {
         long diffInMillies = toDate.getTime() - fromDate.getTime();
-        return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+        return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
+    }
+
+    public static ResponseEntity errorResponse(String message, HttpStatus status) {
+        ObjectNode errorResponse = JsonNodeFactory.instance.objectNode();
+
+        errorResponse.put("error", message);
+
+        return new ResponseEntity<ObjectNode>(errorResponse, status);
     }
 }
