6ee9d729a72fd5f655fa8c5fc5020030a443a210
FoysalOsmany <foysalo@gmail.com>
2019-10-24 19:03:16 +0600

    Refactor parking lot component

--- src/main/java/com/park/parkboot/components/parkinglot/ParkingLotController.java
+++ src/main/java/com/park/parkboot/components/parkinglot/ParkingLotController.java
@@ -1,14 +1,8 @@
 package com.park.parkboot.components.parkinglot;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.park.parkboot.components.car.Car;
-import com.park.parkboot.components.car.CarRepository;
 import com.park.parkboot.utils.Util;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,64 +14,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ParkingLotController {
-    ParkingLotRepository parkingLotRepository;
-    CarRepository carRepository;
     ParkingLotService parkingLotService;
 
     @Autowired
-    public ParkingLotController(ParkingLotRepository parkingLotRepository, CarRepository carRepository,
-            ParkingLotService parkingLotService) {
-        this.parkingLotRepository = parkingLotRepository;
+    public ParkingLotController(ParkingLotService parkingLotService) {
         this.parkingLotService = parkingLotService;
-        this.carRepository = carRepository;
     }
 
     @GetMapping(path = "/parkingLots/{parkingLotNumber}")
     public ResponseEntity<?> parkingLotDetails(@PathVariable("parkingLotNumber") Integer parkingLotNumber) {
-        Optional<ParkingLot> parkingLot = parkingLotRepository.findById(parkingLotNumber);
+        if (parkingLotNumber == null) {
+            return Util.errorResponse("Invalid Request Parameter", HttpStatus.BAD_REQUEST);
+        }
 
-        if (!parkingLot.isPresent()) {
-            return Util.errorResponse("No parking lot found for the parkingLotNumber", HttpStatus.NOT_FOUND);
-        } else {
-            Integer capacity = parkingLot.get().getCapacity();
+        try {
+            ObjectNode data = this.parkingLotService.getParkingLotDetails(parkingLotNumber);
 
-            Integer occupied = carRepository.countByParkingLotParkingLotNumber(parkingLotNumber);
-
-            ObjectNode response = JsonNodeFactory.instance.objectNode();
-
-            response.put("name", parkingLot.get().getName());
-            response.put("capacity", capacity);
-            response.put("parkingLotNumber", parkingLotNumber);
-            response.put("emptySlotCount", capacity - occupied);
-
-            return new ResponseEntity<ObjectNode>(response, HttpStatus.OK);
+            return new ResponseEntity<ObjectNode>(data, HttpStatus.OK);
+        } catch (Exception e) {
+            return Util.errorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping(path = "/parkingLots/{parkingLotNumber}/sameColor")
     public ResponseEntity<?> parkingLotCarsWithSameColor(@PathVariable("parkingLotNumber") Integer parkingLotNumber) {
-        Optional<ParkingLot> parkingLot = parkingLotRepository.findById(parkingLotNumber);
+        if (parkingLotNumber == null) {
+            return Util.errorResponse("Invalid Request Parameter", HttpStatus.BAD_REQUEST);
+        }
 
-        if (!parkingLot.isPresent()) {
-            ObjectNode errorResponse = JsonNodeFactory.instance.objectNode();
-            errorResponse.put("error", "No parking lot found for the parkingLotNumber");
-            return new ResponseEntity<ObjectNode>(errorResponse, HttpStatus.NOT_FOUND);
-        } else {
-            List<Car> cars = parkingLot.get().getCars();
+        try {
+            HashMap<String, List<String>> data = this.parkingLotService.getCarsWithSameColor(parkingLotNumber);
 
-            HashMap<String, List<String>> carColorMap = new HashMap<String, List<String>>();
-
-            cars.forEach(car -> {
-                if (carColorMap.containsKey(car.getColor())) {
-                    carColorMap.get(car.getColor()).add(car.getLicenseNumber());
-                } else {
-                    List<String> licenseList = new ArrayList<String>();
-                    licenseList.add(car.getLicenseNumber());
-                    carColorMap.put(car.getColor(), licenseList);
-                }
-            });
-
-            return new ResponseEntity<HashMap<String, List<String>>>(carColorMap, HttpStatus.OK);
+            return new ResponseEntity<HashMap<String, List<String>>>(data, HttpStatus.OK);
+        } catch (Exception e) {
+            return Util.errorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/park/parkboot/components/parkinglot/ParkingLotService.java
+++ src/main/java/com/park/parkboot/components/parkinglot/ParkingLotService.java
@@ -1,8 +1,15 @@
 package com.park.parkboot.components.parkinglot;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.park.parkboot.components.car.Car;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -56,6 +63,51 @@ public class ParkingLotService {
             }
         default:
             throw new IllegalArgumentException("INVALID parkingRule");
+        }
+    }
+
+    public HashMap<String, List<String>> getCarsWithSameColor(Integer parkingLotNumber) {
+        Optional<ParkingLot> parkingLot = parkingLotRepository.findById(parkingLotNumber);
+
+        if (!parkingLot.isPresent()) {
+            throw new IllegalArgumentException("No parking lot found for the parkingLotNumber");
+        } else {
+            List<Car> cars = parkingLot.get().getCars();
+
+            HashMap<String, List<String>> carColorMap = new HashMap<String, List<String>>();
+
+            cars.forEach(car -> {
+                if (carColorMap.containsKey(car.getColor())) {
+                    carColorMap.get(car.getColor()).add(car.getLicenseNumber());
+                } else {
+                    List<String> licenseList = new ArrayList<String>();
+                    licenseList.add(car.getLicenseNumber());
+                    carColorMap.put(car.getColor(), licenseList);
+                }
+            });
+
+            return carColorMap;
+        }
+    }
+
+    public ObjectNode getParkingLotDetails(Integer parkingLotNumber) {
+        Optional<ParkingLot> parkingLot = parkingLotRepository.findById(parkingLotNumber);
+
+        if (!parkingLot.isPresent()) {
+            throw new IllegalArgumentException("No parking lot found for the parkingLotNumber");
+        } else {
+            Integer capacity = parkingLot.get().getCapacity();
+
+            Integer occupied = capacity - parkingLot.get().getCars().size();
+
+            ObjectNode data = JsonNodeFactory.instance.objectNode();
+
+            data.put("name", parkingLot.get().getName());
+            data.put("capacity", capacity);
+            data.put("parkingLotNumber", parkingLotNumber);
+            data.put("emptySlotCount", capacity - occupied);
+
+            return data;
         }
     }
 }
